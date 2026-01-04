098c4dc0d9f0a2714113784a7af6baafab6602fa
ssabii <fhy1930@gmail.com>
2026-01-05 00:37:52 +0900

    feat: 레이아웃 라우트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { Route, Routes } from "react-router";
+import { Outlet, Route, Routes } from "react-router";
 import IndexPage from "./pages/index-page";
 import SignInPage from "./pages/sign-in-page";
 import SignUpPage from "./pages/sign-up-page";
 
 import "./App.css";
 
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
     </Routes>
   );
 }
