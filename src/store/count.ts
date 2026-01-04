8b36ab45c6059f1ecae5245493a33b85b7cc8e2d
ssabii <fhy1930@gmail.com>
2026-01-05 01:06:56 +0900

    refactor: 카운터 예제 구조 수정

--- src/store/count.ts
+++ src/store/count.ts
@@ -2,12 +2,34 @@ import { create } from "zustand";
 
 interface CountState {
   count: number;
-  increase: () => void;
-  decrease: () => void;
+  actions: {
+    increase: () => void;
+    decrease: () => void;
+  };
 }
 
 export const useCountStore = create<CountState>((set) => ({
   count: 0,
-  increase: () => set((state) => ({ count: state.count + 1 })),
-  decrease: () => set((state) => ({ count: state.count - 1 })),
+  actions: {
+    increase: () => set((state) => ({ count: state.count + 1 })),
+    decrease: () => set((state) => ({ count: state.count - 1 })),
+  },
 }));
+
+export const useCount = () => {
+  const count = useCountStore((state) => state.count);
+
+  return count;
+};
+
+export const useIncreaseCount = () => {
+  const increase = useCountStore((state) => state.actions.increase);
+
+  return increase;
+};
+
+export const useDecreaseCount = () => {
+  const decrease = useCountStore((state) => state.actions.decrease);
+
+  return decrease;
+};
