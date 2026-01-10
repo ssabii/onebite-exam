a2b94ec1ce8ac0392759efe7fcc59f33964bedea
ssabii <fhy1930@gmail.com>
2026-01-10 21:56:42 +0900

    chore: setup tanstack-query

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
