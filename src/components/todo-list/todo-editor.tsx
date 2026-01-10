95712209a7f48696e33d42f104548bf5b1d95135
ssabii <fhy1930@gmail.com>
2026-01-10 21:19:16 +0900

    feat: 투두리스트 기능 구현

--- src/components/todo-list/todo-editor.tsx
+++ src/components/todo-list/todo-editor.tsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useCreateTodo } from "@/store/todos";
+import { useRef, useState, type FormEvent } from "react";
 
 export default function TodoEditor() {
+  const ref = useRef<HTMLInputElement | null>(null);
+  const createTodo = useCreateTodo();
+  const [content, setContent] = useState("");
+
+  const handleAddSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (content.trim() === "") return;
+
+    createTodo(content);
+    setContent("");
+    ref.current?.focus();
+  };
+
   return (
-    <div className="flex gap-2">
-      <Input placeholder="새로운 할 일을 입력하세요 ..." />
-      <Button>추가</Button>
-    </div>
+    <form className="flex gap-2" onSubmit={handleAddSubmit}>
+      <Input
+        ref={ref}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="새로운 할 일을 입력하세요 ..."
+      />
+      <Button type="submit">추가</Button>
+    </form>
   );
 }
